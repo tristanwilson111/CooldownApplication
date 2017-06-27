04b606512dc2ab894297c63d34ff43378b84ad3e
trist <tristanwilson111@gmail.com>
2017-06-27 18:03:59 -0400

    initv2

---
+++ app/src/main/java/com/icaninteractive/tristan/cooldownapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.icaninteractive.tristan.cooldownapplication;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
