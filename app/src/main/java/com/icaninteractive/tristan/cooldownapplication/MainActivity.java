6fd030dccf1ff7859329197a83ef5ee944b33ef4
trist <tristanwilson111@gmail.com>
2017-08-23 01:04:32 -0400

    Repositioned GIF, TextView to proper place. Made application fullscreen on open.

--- app/src/main/java/com/icaninteractive/tristan/cooldownapplication/MainActivity.java
+++ app/src/main/java/com/icaninteractive/tristan/cooldownapplication/MainActivity.java
@@ -6,9 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
 
